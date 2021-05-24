e5369b91b46f0675a086093c292451612ddd69d4
Lasith Koswatta Gamage <lasithvindu1@gmail.com>
2021-05-24 17:26:20 +1000

    Heartsight Logo

--- src/components/pages/core/Navbar.js
+++ src/components/pages/core/Navbar.js
@@ -2,7 +2,10 @@ import React from 'react'
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import FavoriteIcon from '@material-ui/icons/Favorite';
 import { makeStyles } from '@material-ui/core';
+import Box from '@material-ui/core/Box';
 
 const drawerWidth = 240;
 
@@ -10,7 +13,12 @@ const useStyles = makeStyles((theme)=>({
     appBar: {
         zIndex: theme.zIndex.drawer + 1,
         backgroundColor: "#6200EE",
-      }
+        fontWeight: "bold"
+    },
+    icon: {
+      marginRight: "0.8rem",
+      color: "red"
+    }
 }
 ))
 
@@ -20,9 +28,19 @@ export const Navbar = () => {
     return (
       <AppBar position="fixed" className={classes.appBar}>
         <Toolbar>
-          <Typography variant="h6" noWrap>
-            Dashboard
-          </Typography>
+          <Grid
+                justify="space-between" // Add it here :)
+                container >
+            <Typography variant="h6" noWrap>
+              Dashboard
+            </Typography>
+            <Box display="flex" alignItems="center">
+              <FavoriteIcon className={classes.icon}/>
+              <Typography variant="h6" noWrap>
+                Heartsight
+              </Typography>
+            </Box>
+          </Grid>
         </Toolbar>
       </AppBar>
     )
