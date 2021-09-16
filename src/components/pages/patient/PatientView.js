e929f052dbd54b449f5aaa486868cbe6d4cf12e3
mpham21 <matthew.hv.pham@gmail.com>
2021-09-16 23:47:11 +1000

    Implemented patient list view and unlink patient

--- src/components/pages/patient/PatientView.js
+++ src/components/pages/patient/PatientView.js
@@ -6,81 +6,97 @@ import { Typography, Button } from "@material-ui/core";
 import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 import Tabs from "../../Tabs";
 import { useLocation } from "react-router-dom";
+import { firestore } from "../../../firebase";
+import { useAuth } from "../../../contexts/AuthContext";
+import { useHistory } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: "center",
-    color: theme.palette.text.secondary,
-    height: "100%",
-  },
+    root: {
+        flexGrow: 1,
+    },
+    paper: {
+        padding: theme.spacing(2),
+        textAlign: "center",
+        color: theme.palette.text.secondary,
+        height: "100%",
+    },
 }));
 
 const PatientLister = (props) => {
-  const location = useLocation();
-  const data = location.state;
-  const tabHeadings = ["Alerts", "ECG History", "Heartrate"];
-  const fields = [
-    { field: "email", title: "Email" },
-    { field: "occupation", title: "Occupation" },
-    { field: "age", title: "Age" },
-    { field: "gender", title: "Gender" },
-    { field: "phone", title: "Phone" },
-    { field: "maritalStatus", title: "Marital Status" },
-  ];
-  const classes = useStyles();
-  return (
-    <>
-      <div className={classes.root}>
-        <Grid container spacing={3}>
-          <Grid item xs={2}>
-            <Paper className={classes.paper}>
-              <AccountCircleIcon fontSize="large" />
-            </Paper>
-          </Grid>
-          <Grid item xs={2}>
-            <Paper className={classes.paper}>
-              <Grid container spacing={3}>
-                <Grid item xs={12}>
-                  <Typography variant="h5">Patient</Typography>
+    const location = useLocation();
+    const data = location.state;
+    const tabHeadings = ["Alerts", "ECG History", "Heartrate"];
+    const fields = [
+        { field: "email", title: "Email" },
+        { field: "occupation", title: "Occupation" },
+        { field: "age", title: "Age" },
+        { field: "gender", title: "Gender" },
+        { field: "phone", title: "Phone" },
+        { field: "maritalStatus", title: "Marital Status" },
+    ];
+    const classes = useStyles();
+    const { currentUser } = useAuth();
+    const history = useHistory();
+    const unlinkPatient = () =>  {
+        firestore.collection(`/doctors/${currentUser.uid}/linkedPatients`).doc(data.id).delete().then(
+            () => {
+                history.push({ pathname: "/patients", state: {} });
+                console.log("deleted")
+            }
+    
+        )
+
+    };
+    return (
+        <>
+            <div className={classes.root}>
+                <Grid container spacing={3}>
+                    <Grid item xs={2}>
+                        <Paper className={classes.paper}>
+                            <AccountCircleIcon fontSize="large" />
+                        </Paper>
+                    </Grid>
+                    <Grid item xs={2}>
+                        <Paper className={classes.paper}>
+                            <Grid container spacing={3}>
+                                <Grid item xs={12}>
+                                    <Typography variant="h5">Patient</Typography>
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <Typography>
+                                        {data["givenName"]} {data["surname"]}
+                                    </Typography>
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <Button
+                                        style={{ backgroundColor: "red", color: "white" }}
+                                        onClick={() => unlinkPatient()}
+                                    >
+                                        Unlink Patient
+                                    </Button>
+                                </Grid>
+                            </Grid>
+                        </Paper>
+                    </Grid>
+                    <Grid item xs={8}>
+                        <Paper className={classes.paper}>
+                            <Grid container spacing={3} justify="center">
+                                {fields.map((field, key) => (
+                                    <Grid key={key} item xs={6}>
+                                        <Typography>
+                                            {field.title}: {data[field.field]}
+                                        </Typography>
+                                    </Grid>
+                                ))}
+                            </Grid>
+                        </Paper>
+                    </Grid>
                 </Grid>
-                <Grid item xs={12}>
-                  <Typography>
-                    {data["givenName"]} {data["surname"]}
-                  </Typography>
-                </Grid>
-                <Grid item xs={12}>
-                  <Button
-                    style={{ backgroundColor: "#6200EE", color: "white" }}
-                  >
-                    See Profile
-                  </Button>
-                </Grid>
-              </Grid>
-            </Paper>
-          </Grid>
-          <Grid item xs={8}>
-            <Paper className={classes.paper}>
-              <Grid container spacing={3} justify="center">
-                {fields.map((field, key) => (
-                  <Grid key={key} item xs={6}>
-                    <Typography>
-                      {field.title}: {data[field.field]}
-                    </Typography>
-                  </Grid>
-                ))}
-              </Grid>
-            </Paper>
-          </Grid>
-        </Grid>
-        <div style={{ height: 20 }}></div>
-        <Tabs headers={tabHeadings} />
-      </div>
-    </>
-  );
+                <div style={{ height: 20 }}></div>
+                <Tabs headers={tabHeadings} />
+            </div>
+        </>
+    );
 };
 
 export default PatientLister;
